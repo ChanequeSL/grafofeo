561ee86f2858f8186b589ead6d155c0320ab90e0
VictorGarduno <ve15421@innovaccion.mx>
2022-01-15 00:53:37 -0700

    Actualizacion de Nodo

--- src/Grafo/Nodo.java
+++ src/Grafo/Nodo.java
@@ -1,14 +1,44 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Grafo;
-
-/**
- *
- * @author nekokawai99
- */
 public class Nodo {
+   private String info;
+   private boolean arista;
+   private int[] enlaces;
+   private int num=0;
+   public Nodo(String info){
+      this.info = info;
+      num+=1;
+   }
+
+   public String getInfo() {
+      return info;
+   }
+
+   public void setInfo(String info) {
+      this.info = info;
+   }
+
+   public boolean isArista() {
+      return arista;
+   }
+
+   public void setArista(boolean arista) {
+      this.arista = arista;
+   }
+
+   public int[] getEnlaces() {
+      return enlaces;
+   }
+
+   public void setEnlaces(int[] enlaces) {
+      this.enlaces = enlaces;
+   }
+
+   public int getNum() {
+      return num;
+   }
+
+   public void setNum(int num) {
+      this.num = num;
+   }
    
 }
