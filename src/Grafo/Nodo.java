79cd285cbc5326ea471216df55653d0e9c83c5d7
VictorGarduno <ve15421@innovaccion.mx>
2022-01-15 00:06:13 -0700

    Añadi clase nodo

---
+++ src/Grafo/Nodo.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Grafo;
+
+/**
+ *
+ * @author nekokawai99
+ */
+public class Nodo {
+   
+}
